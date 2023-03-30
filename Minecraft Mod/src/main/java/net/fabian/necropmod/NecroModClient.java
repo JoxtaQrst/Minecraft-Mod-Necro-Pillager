95c2fdd9766603b8ac190ce1655913fee37d15fa
JoxtaQrst <pintea2002@gmail.com>
2023-03-30 18:32:09 +0300

    Initial commit

---
+++ Minecraft Mod/src/main/java/net/fabian/necropmod/NecroModClient.java
@@ -0,0 +1,10 @@
+package net.fabian.necropmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NecroModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
